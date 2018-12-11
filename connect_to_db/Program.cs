7a038f848d0f40571f17c071caf5f39d15b83bbf
Oleksandr Finiuk <olexandr.finyuk@gmail.com>
2018-12-11 22:01:07 +0200

    create connection

--- connect_to_db/Program.cs
+++ connect_to_db/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace connect_to_db
 {
@@ -10,6 +14,81 @@ namespace connect_to_db
     {
         static void Main(string[] args)
         {
+            string conStr = ConfigurationManager.AppSettings["ConnectionString"];
+            SqlConnection con = new SqlConnection(conStr);
+
+            UserProvider up = new UserProvider(con);
+            bool success = up.Login("mail@mail", "123456");
+            Console.WriteLine("success = {0}", success);
+
+            string mail = "mail@mail.com";
+            Console.WriteLine($"\n{mail} is {EmailChecker(mail)}\n");
+
+            string[] pass = { "Qwer!", "Qwerty1!", "123456", "qwertyu1", "Qwerty1 " };
+            for (int i = 0; i < pass.Length; i++)
+                //Console.WriteLine($"{pass[i],10} - {PasswordChecker(pass[i])}");
+                Console.WriteLine($"{pass[i],10} - {IsValidPassword(pass[i])}");
+
+            con.Close();
+        }
+
+        static bool EmailChecker(string email)
+        {
+            MailAddress mail = new MailAddress(email);
+            if (mail.Address == email)
+                return true;
+            else
+                return false;
+        }
+
+        static bool PasswordChecker(string pass)
+        {
+            if (pass.Length < 6)
+            {
+                //Console.WriteLine("Is to short!");
+                return false;
+            }
+
+            bool Up = false;
+            bool Low = false;
+            bool Digit = false;
+            bool Symb = false;
+
+            foreach (char c in pass)
+            {
+                if (char.IsUpper(c))
+                    Up = true;
+                else
+                    if (char.IsLower(c))
+                    Low = true;
+                else
+                    if (char.IsDigit(c))
+                    Digit = true;
+                else
+                    if (char.IsPunctuation(c))
+                    Symb = true;
+                //Console.WriteLine($"{Up}\t{Low}\t{Digit}\t{Symb}");
+            }
+
+            if (Up && Low && Digit && Symb)
+            {
+                return true;
+            }
+            else
+            {
+                //Console.WriteLine("Wrong password");
+                return false;
+            }
+
+
+
+
+        }
+        public static bool IsValidPassword(string plainText)
+        {
+            Regex regex = new Regex(@"^(.{0,7}|[^0-9]*|[^A-Z])$");
+            Match match = regex.Match(plainText);
+            return match.Success;
         }
     }
 }
---
+++ connect_to_db/UserProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace connect_to_db
+{
+    class UserProvider
+    {
+        private SqlConnection _con;
+
+        public UserProvider(SqlConnection con)
+        {
+            _con = con;
+        }
+
+        public bool Login(string email, string password)
+        {
+            string conToDb = "Data Source=somebase.mssql.somee.com;Initial Catalog=somebase;User ID=finiuk_SQLLogin_1;Password=mvkwivz38h";
+
+            using (SqlConnection con = new SqlConnection(conToDb))
+            {
+                con.Open();
+                string query = $"Select * from tblUsers";
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = con;
+                sqlCommand.CommandText = query;
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (reader["Email"].ToString() == email && reader["Password"].ToString() == password)
+                        return true;
+                }
+                return false;
+            }
+
+        }
+    }
+}
